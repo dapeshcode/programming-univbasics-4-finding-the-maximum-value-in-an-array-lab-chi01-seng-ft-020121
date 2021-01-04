4ead11292b8cda21a45a7bbff4db30f8e911f3e8
Alexandra Peccia <ampeccia@gmail.com>
2021-01-04 02:48:55 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,5 +1,15 @@
-
-
+def find_max_value(array)
+  count = 0 
+  while count < array.length do 
+    preceding = array[count] 
+    count += 1 
+      if preceding > array[count]
+        max_value = preceding
+      end 
+    end 
+    
+    
+end 
 
 
 # def find_max_value(array)
